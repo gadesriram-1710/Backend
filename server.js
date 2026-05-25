722d4fdff8b6951b182496773a367483078c8188
gadesriram-1710 <gadesriram1710@gmail.com>
2026-05-25 11:26:44 +0530

    Fixed Render deployment

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
-const express = require('express');
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
-const cors = require('cors');
+const connectDB = require("./config/DbConnect");
+const productsRoutes = require("./routes/productsRouter");
 
-const connectDB = require('./config/DbConnect');
-const productsRoutes = require('./routes/productsRouter');
-
+// Middleware
+app.use(cors());
 app.use(express.json());
 
-
+// Database Connection
 connectDB();
 
+// Routes
 app.use(productsRoutes);
 
-app.listen(5000,()=>{
-   console.log("server started");
+// Default Route
+app.get("/", (req, res) => {
+  res.send("Backend Running Successfully");
+});
+
+// Render Dynamic Port
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
